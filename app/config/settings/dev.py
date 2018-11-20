7edb492c16ab5444fa10331d93f2e33dc681c17b
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-20 15:43:28 +0900

    aws s3, aws rds setup 완료

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -2,9 +2,20 @@ from .base import *
 
 DEBUG = True
 
+# SECRETS
+secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
+
+# DATABASE
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# STORAGE
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -7,3 +7,11 @@ secrets = json.load(open(os.path.join(SECRET_DIR, 'production.json')))
 
 # DATABASE
 DATABASES = secrets['DATABASES']
+
+# STORAGE
+AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+
