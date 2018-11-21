3ddb643f6cc6dc6278e8ac47d143f1b734d9a2e1
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-21 22:59:22 +0900

    dev database sqlite3로 변경, related_query_name 설정

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -9,7 +9,12 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 secrets = json.load(open(os.path.join(SECRET_DIR, 'dev.json')))
 
 # DATABASE
-DATABASES = secrets['DATABASES']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # STORAGE
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
--- app/home/models.py
+++ app/home/models.py
@@ -12,21 +12,35 @@ class Room(models.Model):
     beds = models.IntegerField()
     city = models.CharField(max_length=50)
     person_capacity = models.IntegerField()
-    host_thumbnail_url=models.ImageField(upload_to='pictures/host/', null=True)
-    host_thumbnail_url_small=models.ImageField(upload_to='pictures/host/', null=True)
+    host_thumbnail_url=models.ImageField(upload_to='pictures/host/', blank=True, null=True)
+    host_thumbnail_url_small=models.ImageField(upload_to='pictures/host/', blank=True, null=True)
     lat = models.FloatField()
     lng = models.FloatField()
     room_name = models.CharField(max_length=200)
     room_type = models.CharField(max_length=1, choices=ROOM_TYPES)
     price = models.IntegerField()
-    amenities = models.ForeignKey('Amenity', on_delete=models.SET_NULL, null=True)
-    house_img_urls = models.ForeignKey('HouseImgs', on_delete=models.SET_NULL, null=True)
+    amenities = models.ForeignKey('Amenity', on_delete=models.SET_NULL, null=True, related_query_name='room_amenities')
 
+    def __str__(self):
+        return self.room_name
 
 class Amenity(models.Model):
+    room = models.ForeignKey('Room',
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             )
     title = models.CharField(max_length=30)
 
 
+    def __str__(self):
+        return self.title
+
+
 class HouseImgs(models.Model):
+    room = models.ForeignKey('Room',
+                             on_delete=models.CASCADE,
+                             related_name='room_house_imgs',)
     url = models.URLField()
 
+    def __str__(self):
+        return self.room
