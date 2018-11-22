8c7329a4874d3893287d194b3aabe1bc7e7c7800
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-22 16:32:08 +0900

    http redirect to https

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -11,7 +11,6 @@ ALLOWED_HOSTS = [
     "www.backends.xyz",
     "api.backends.xyz",
     ".elasticbeanstalk.com",
-    "localhost",
 ]
 
 # DATABASE
@@ -26,3 +25,35 @@ AWS_S3_REGION_NAME = 'ap-northeast-2'
 
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
