443adccadd36424001cd9e1d9cff7f5f2a2769b9
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-27 16:29:17 +0900

    djangoextensions 제거

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
-    'django_extensions',
+    # 'django_extensions',
     # 'debug_toolbar',
 ]
 
