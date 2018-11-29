9ffbe6e4d3ca2ee14940089486dbb1e9f3d5d6ad
seongwonhan88 <seongwonhan88@gmail.com>
2018-11-29 15:56:33 +0900

    media storage permission public-read로 변경

--- app/config/storages.py
+++ app/config/storages.py
@@ -3,7 +3,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
-    default_acl = 'private'
+    default_acl = 'public-read'
 
 class StaticStorage(S3Boto3Storage):
     location = 'static'
