f9041393d48db4c967aa3e0eb83754b0f0305ec4
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-03 18:36:26 +0900

    host model field 추가

--- app/home/urls.py
+++ app/home/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .apis import RoomListingApiView, RoomDetailApiView, RoomApiView, BookingAPIView, BookingDateAPIView
+from .apis import RoomListingApiView, RoomDetailApiView, RoomApiView, BookingAPIView
 
 urlpatterns = [
     # path('listings/', RoomListingApiView.as_view(), name='room-list'),
     path('listings/<int:pk>/', RoomDetailApiView.as_view(), name='room-detail'),
     path('listings/', RoomApiView.as_view(), name='room-list-generic'),
     path('booking/', BookingAPIView.as_view(), name='booking'),
-    path('booking/<int:booking_id>/', BookingAPIView.as_view(), name='booking-del'),
 ]
