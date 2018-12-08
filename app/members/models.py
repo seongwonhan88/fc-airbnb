9b15a954e1e18fe6497791d1f625682d92b3f141
seongwonhan88 <seongwonhan88@gmail.com>
2018-12-08 13:41:30 +0900

    user model 분리 마이그레이션 미적용

--- app/members/models.py
+++ app/members/models.py
@@ -1,14 +1,32 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 
+class NormalUserManager(UserManager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_host=False)
+
+
+class HostUserManager(UserManager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_host=True)
+
+
 class User(AbstractUser):
+    #매니저 설정
+    objects = models.Manager
+    user_objects = NormalUserManager
+    host_objects = HostUserManager
+
+    #공통 필드
     img_profile = models.ImageField(upload_to='user', blank=True)
+
+    # USER 필드
     user_introduction = models.TextField(blank=True)
     saved_room = models.ManyToManyField('home.Room', related_name='saved_user')
+
+    # HOST 관련 필드
     is_host = models.BooleanField(default=False)
-
-
-class Host(User):
     rating = models.PositiveIntegerField(default=4)
     listings = models.ManyToManyField('home.Room', related_name='hosting')
+    host_introduction = models.TextField(blank=True, null=True)
