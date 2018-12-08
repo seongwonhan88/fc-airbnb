0f98d7585c9e06c45e05db8fbffb49b3bb962139
gs2dang <gs2dang07@gmail.com>
2018-12-08 18:53:06 +0900

    RoomReviewListAPIView 추가

--- app/home/urls.py
+++ app/home/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .apis import RoomDetailApiView, RoomApiView, BookingAPIView, BookingCancelAPIView, ReviewAPIView, ReviewListAPIView
-
+from .apis import RoomDetailApiView, RoomApiView, BookingAPIView, BookingCancelAPIView, ReviewAPIView, \
+    UserReviewListAPIView, RoomReviewListAPIView
 
 urlpatterns = [
     path('listings/<int:pk>/', RoomDetailApiView.as_view(), name='room-detail'),
     path('listings/', RoomApiView.as_view(), name='room-list-generic'),
     path('booking/', BookingAPIView.as_view(), name='booking'),
     path('booking_cancel/', BookingCancelAPIView.as_view(), name='booking-cancel'),
-    path('review/list/', ReviewListAPIView.as_view(), name='review-list'),
+    path('review/user/list/', UserReviewListAPIView.as_view(), name='review-user-list'),
+    path('review/room/list/', RoomReviewListAPIView.as_view(), name='review-room-list'),
     path('review/<int:room_id>/', ReviewAPIView.as_view(), name='review'),
 ]
